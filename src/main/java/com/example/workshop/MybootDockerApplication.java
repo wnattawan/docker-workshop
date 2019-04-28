16c0f589060a5d4156869b71eb38859132971180
Nattawan Jakobsson <nattawan.jakobsson@bisnode.com>
2019-04-28 13:24:38 +0200

    Initial commit

---
+++ src/main/java/com/example/workshop/MybootDockerApplication.java
@@ -0,0 +1,13 @@
+package com.example.workshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MybootDockerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MybootDockerApplication.class, args);
+    }
+
+}
